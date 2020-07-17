8165f2480f8124a34358324ecd8d672966b35f54
Felix C. Stegerman <flx@obfusk.net>
2020-07-17 23:59:20 +0200

    android: fix python3 recipe

--- android/p4a-recipes/python3/__init__.py
+++ android/p4a-recipes/python3/__init__.py
@@ -1,2 +1,11 @@
-from ._python3 import Python3Recipe, recipe
-Python3Recipe.configure_args += ("--enable-loadable-sqlite-extensions",)
+def _patch():
+  global Python3Recipe, recipe
+  import importlib.util as IU, os
+  name  = "pythonforandroid.recipes._python3"
+  file  = os.path.join(os.path.dirname(__file__), "_python3.py")
+  spec  = IU.spec_from_file_location(name, file)
+  mod   = IU.module_from_spec(spec)
+  spec.loader.exec_module(mod)
+  Python3Recipe, recipe = mod.Python3Recipe, mod.recipe
+  Python3Recipe.configure_args += ("--enable-loadable-sqlite-extensions",)
+_patch()
