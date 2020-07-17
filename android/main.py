ba0d2a9ae9e29436e0ac216609199bca08b4b970
Felix C. Stegerman <flx@obfusk.net>
2020-07-17 22:22:54 +0200

    android: work around p4a/click stdio conflict

--- android/main.py
+++ android/main.py
@@ -1,2 +1,19 @@
+import sys
+if isinstance(getattr(sys.stdout, "buffer", None), str):
+  print("Fixing stdout/stderr...")
+  import androidembed
+  class LogFile:
+    def __init__(self):
+      self.__buf = ""
+    def write(self, s):
+      s = self.__buf + s
+      lines = s.split("\n")
+      for l in lines[:-1]:
+        androidembed.log(l)
+      self.__buf = lines[-1]
+    def flush(self):
+      return
+  sys.stdout = sys.stderr = LogFile()
+
 from jiten.cli import cli
 cli("-v serve".split())
