4f24506ab0ce672c7c978127eb8cb05d29bf8ed8
Felix C. Stegerman <flx@obfusk.net>
2020-08-05 11:27:42 +0200

    android: update setuptools

--- android/p4a-recipes/setuptools/__init__.py
+++ android/p4a-recipes/setuptools/__init__.py
@@ -1,7 +1,7 @@
 from pythonforandroid.recipe import PythonRecipe
 
 class SetuptoolsRecipe(PythonRecipe):
-    version = '49.2.0'
+    version = '49.2.1'
     url = 'https://pypi.python.org/packages/source/s/setuptools/setuptools-{version}.zip'
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
