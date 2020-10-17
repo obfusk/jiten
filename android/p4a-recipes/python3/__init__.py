f1c2af729b6fed1e30efb87c6d14883297192296
Felix C. Stegerman <flx@obfusk.net>
2020-10-17 16:57:40 +0200

    update python to 3.9.0

--- android/p4a-recipes/python3/__init__.py
+++ android/p4a-recipes/python3/__init__.py
@@ -8,5 +8,5 @@ def _patch():
   spec.loader.exec_module(mod)
   Python3Recipe, recipe         = mod.Python3Recipe, mod.recipe
   Python3Recipe.configure_args += ("--enable-loadable-sqlite-extensions",)
-  Python3Recipe.version         = "3.8.6"
+  Python3Recipe.version         = "3.9.0"
 _patch()
