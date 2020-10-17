0882e2d29b7888c2858b5dc7ab72586d527a47bf
Felix C. Stegerman <flx@obfusk.net>
2020-10-17 18:16:11 +0200

    android: fix python3 build

--- android/p4a-recipes/python3/__init__.py
+++ android/p4a-recipes/python3/__init__.py
@@ -6,7 +6,16 @@ def _patch():
   spec  = IU.spec_from_file_location(name, file)
   mod   = IU.module_from_spec(spec)
   spec.loader.exec_module(mod)
-  Python3Recipe, recipe         = mod.Python3Recipe, mod.recipe
-  Python3Recipe.configure_args += ("--enable-loadable-sqlite-extensions",)
-  Python3Recipe.version         = "3.9.0"
+  sqlite_ext  = "--enable-loadable-sqlite-extensions"
+  patches     = [
+    "patches/py3.8.1.patch",
+    "patches/py3.8.1_fix_cortex_a8.patch"
+  ]
+  Python3Recipe, recipe = mod.Python3Recipe, mod.recipe
+  Python3Recipe.version = "3.9.0"
+  if sqlite_ext not in Python3Recipe.configure_args:
+    Python3Recipe.configure_args += (sqlite_ext,)
+  for patch in patches:
+    if patch not in Python3Recipe.patches:
+      Python3Recipe.patches.append(patch)
 _patch()
