4e193cdc08141d4c5a2135bae03531aaae779724
Felix C. Stegerman <flx@obfusk.net>
2021-01-23 20:05:30 +0100

    android: pull in buildozer.sh

--- android/scripts/buildozer.sh
+++ android/scripts/buildozer.sh
@@ -8,9 +8,11 @@ buildozer android p4a -- --version  # install requirements
 dir="$PWD" p=( patch -N -r- -p1 )
 (
   cd .p4a
-  git status && git checkout -- . && git clean -f && git status
+  git status ; git checkout -- . ; git clean -f ; git status
   if [ -n "$tags" ]; then
     git reset --hard "$commit"  # pin p4a commit for releases (tags)
+  else
+    git pull --rebase
   fi
   for f in "$dir"/patches/*.patch; do
     echo "applying patch $(basename "$f") ..."
