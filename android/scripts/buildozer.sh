7fd015c975af84f9a4ac9a7420a36fa91c941f31
Felix C. Stegerman <flx@obfusk.net>
2021-03-08 07:53:15 +0100

    android: always pin p4a

--- android/scripts/buildozer.sh
+++ android/scripts/buildozer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
-tags="$( git tag --points-at HEAD )" commit="$( < p4a-commit )"
+commit="$( < p4a-commit )"
 
 mkdir -p ../../_jiten_buildozer_
 buildozer android p4a -- --version  # install requirements
@@ -9,11 +9,7 @@ dir="$PWD" p=( patch -N -r- -p1 )
 (
   cd .p4a
   git status ; git checkout -- . ; git clean -f ; git status
-  if [ -n "$tags" ]; then
-    git reset --hard "$commit"  # pin p4a commit for releases (tags)
-  else
-    git pull --rebase
-  fi
+  git fetch --all ; git reset --hard "$commit"  # pin p4a commit
   for f in "$dir"/patches/*.patch; do
     echo "applying patch $(basename "$f") ..."
     err="$( "${p[@]}" --dry-run < "$f" 2>&1 | grep -iF failed || true )"
